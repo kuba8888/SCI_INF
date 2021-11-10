2d282a34ee28aeb6aceac0fa85b711cacfdd9756
kuba8888 <ja2004kub@gmail.com>
2021-11-10 14:25:37 +0100

    added decryption

--- PracaNaLekcjiNr1/main.cpp
+++ PracaNaLekcjiNr1/main.cpp
@@ -7,24 +7,43 @@ class cipher
 	public:
 		char text[1000],ch;
 		int lenght;
+		int key = 333;
 	void read()
 	{
 		cout << "Write a message that you want to encrypt: ";
 		cin.getline(text, 1000);
 		lenght = strlen(text);
 	}
-	void encrypt()
+	void encryptCaesar()
 	{
 		for (int i = 0; i<lenght;i++)
 		{
 			ch = text[i];
 			if (ch >='a' && ch <='z')
 			{
-				ch = ch + 13;
+				ch = ch + key;
 			}
 			if (ch > 'z')
 			{
 				ch = ch - 'z' + 'a' - 1;
+			}
+			text[i] = ch;
+		}
+		printf("Encrypted message: %s \n", text);
+	}
+	void decryptCaesar()
+	{
+		char ch;
+		for (int i = 0; i<lenght;i++)
+		{
+			ch = text[i];
+			if (ch >='a' && ch <='z')
+			{
+				ch = ch - key;
+			}
+			if (ch < 'a')
+			{
+				ch = ch + 'z' - 'a' + 1;
 			}
 			text[i] = ch;
 		}
@@ -39,6 +58,8 @@ int main()
 {
 	cipher obj;
 	obj.read();
-	obj.encrypt();
+	obj.encryptCaesar();
+	obj.read();
+	obj.decryptCaesar();
 	return 0;
 }
