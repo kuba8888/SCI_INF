d1fdfc25317bf23c1f4f25d21d4b6ea9cb5a5f4d
kuba8888 <ja2004kub@gmail.com>
2021-11-10 01:11:29 +0100

    added cesar cipher

---
+++ PracaNaLekcjiNr1/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class cipher
+{
+	public:
+		char text[1000],ch;
+		int lenght;
+	void read()
+	{
+		cout << "Write a message that you want to encrypt: ";
+		cin.getline(text, 1000);
+		lenght = strlen(text);
+	}
+	void encrypt()
+	{
+		for (int i = 0; i<lenght;i++)
+		{
+			ch = text[i];
+			if (ch >='a' && ch <='z')
+			{
+				ch = ch + 13;
+			}
+			if (ch > 'z')
+			{
+				ch = ch - 'z' + 'a' - 1;
+			}
+			text[i] = ch;
+		}
+		printf("Encrypted message: %s \n", text);
+	}
+	/*void print()
+	{
+		cout << "Encrypted message: ";
+		for(int i = 0; i < lenght; i++)
+		{
+			cout << text[i];
+		}
+	}*/
+
+};
+
+
+
+int main()
+{
+	cipher obj;
+	obj.read();
+	obj.encrypt();
+	return 0;
+}
